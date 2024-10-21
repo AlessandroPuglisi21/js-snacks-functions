0f86db369cfee78598ff70e6bf48a4dbf13f59e6
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-21 16:49:33 +0200

    ADDED: Name

--- snack-funzioni/04 - saluti/script.js
+++ snack-funzioni/04 - saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
-
+const space = ' '
 
 // Dichiara la funzione qui.
-
+function saluto(name){
+    return "Ciao" + space + name
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluto('Mario'))
+console.log(saluto('Antonio'))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

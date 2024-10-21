69628e481612731c2218ea498e33c928c92171eb
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-21 17:11:59 +0200

    ADDED: ora

--- snack-funzioni/05 - bonus/script.js
+++ snack-funzioni/05 - bonus/script.js
@@ -6,13 +6,28 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+const space = ' '
+const orario = prompt('Seleziona Ora')
 
 // Dichiara la funzione qui.
+function saluto(name){
+    const ora = parseFloat(orario)
+
+    if(orario >= 5 && ora< 13){
+        return 'Buongiorno' + space + name
+    } else if (orario >= 13 && ora < 17)
+        return 'Buonpomeriggio' + space + name
+    else if (orario >= 24){
+        return 'Ora non valida'
+    }
+    else 
+    return 'Buonasera' + space + name
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto(name))
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
